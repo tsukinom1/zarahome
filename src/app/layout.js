ce32aefc0c07c1d4ab88d4125b6c69d7d19caf96
tsukinom1 <tsukinom1>
2025-03-22 13:46:03 +0500

    last

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import "../styles/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import LoginContextProvider from "@/utils/contexts/LoginContext";
-import CartContextProvider from "@/utils/contexts/CartContext";
+import { LoginContextProvider } from "@/utils/contexts/LoginContext";
+import { CartContextProvider } from "@/utils/contexts/CartContext";
 import Head from "next/head";
 
 export default function RootLayout({ children }) {
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
             <title>ZaraHome</title>
         </Head>
         <body className="flex flex-col justify-between min-h-screen">
-        <LoginContextProvider value={{ user: null }}>
+        <LoginContextProvider>
             <CartContextProvider>
                 <Header />
                 <main className="flex-1">{children}</main>
