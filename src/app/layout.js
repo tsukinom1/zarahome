182d2ff2fc49d3a71df15d0a98a2306c4d3c9b6f
tsukinom1 <tsukinom1>
2025-03-22 13:42:08 +0500

    export

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import "../styles/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { LoginContextProvider } from "@/utils/contexts/LoginContext";
-import { CartContextProvider } from "@/utils/contexts/CartContext";
+import LoginContextProvider from "@/utils/contexts/LoginContext";
+import CartContextProvider from "@/utils/contexts/CartContext";
 import Head from "next/head";
 
 export default function RootLayout({ children }) {
